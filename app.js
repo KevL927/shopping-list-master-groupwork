9babb590ec2ae4ff118307c686d867e5f6d04c2b
Kevin Lee <KevL927@users.noreply.github.com>
2016-08-23 19:25:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,53 @@
-// enter item in the text box
-
-// click add item or hit return, a box with border line will be created, including the name of the item and two buttons (check and delete)
-
-// when user click check button
-// - the item should be strikethrough
-// - the button should be renamed as ‘uncheck’
-
-// when user clicks on uncheck button
-// - the strikethrough should be gone
-// - the button should be renamed as ‘check’
-
-// when user clicks on delete button:
-//  - the whole entire box for the item (including the buttons) will be removed from the list
-// Shopping List Extension Tasks
-
-// If you are looking for a further challenge, then try add some of the following features to your shopping list application:
-
-// Buttons which act as a filter, allowing you to only display the items which need checking off, or have already been checked off.
-// A text input which lets you search for items which match a specific string.
-// A list of your most frequently entered items, so you can quickly select from your favorites.
-// Use the Web Storage API to keep track of the items in your list.
-
 'use strict'
-var checkButton;
+var checkButton,
+    deletedItems = [];
 
 $(document).ready(function() {
+  var itemTemplate = $('<li><span class="shopping-item"></span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></li>');
+  var shoppingList = $('.shopping-list')
+
+  // event handlers
+
+
     $('#js-shopping-list-form').submit(function(event) {
         event.preventDefault();
-        var item = $('#shopping-list-entry').val();
-
-        if (item.length > 0) {
-            $('.shopping-list').append('<li>' +
-                '<span class="shopping-item">' + item + '</span>' +
-                '<div class="shopping-item-controls">' +
-                '<button class="shopping-item-toggle">' +
-                '<span class="button-label">check</span>' +
-                '</button>' +
-                '<button class="shopping-item-delete">' +
-                '<span class="button-label">delete</span>' +
-                '</button>' +
-                '</li>');
-            $('#shopping-list-entry').val('');
+        if (!$('#shopping-list-entry').val()) {
+          return;
         }
 
+        var currentItem = itemTemplate.clone(),
+            currentShoppingItem = currentItem.find('.shopping-item');
+
+        currentShoppingItem.text($('#shopping-list-entry').val());
+        initializeItem(currentItem);
     });
 
-    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-        $(this).closest('li').find('.shopping-item').toggleClass('.shopping-item shopping-item__checked');
+    function initializeItem(currentItem) {
+      var currentShoppingItem = currentItem.find('.shopping-item');
+      var checkButton = currentItem.find('.shopping-item-toggle .button-label');
 
-        checkButton = $(this).closest('.shopping-item-toggle').find('.button-label');
-        (checkButton.text() === 'check') ? checkButton.text('uncheck'): checkButton.text('check');
-    })
+      function toggleItem (event, currentItem) {
+          currentShoppingItem.toggleClass('.shopping-item shopping-item__checked');
+          (checkButton.text() === 'check') ? checkButton.text('uncheck'): checkButton.text('check');
+      }
+      function deleteItem(event) {
+          currentItem.remove();
+          deletedItems.push(currentItem);
+      }
 
-    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-        $(this).closest('li').remove();
+
+      shoppingList.append(currentItem);
+
+
+      $('.shopping-item-toggle', currentItem).on('click', toggleItem);
+      $('.shopping-item-delete', currentItem).on('click', deleteItem);
+
+
+      $('#shopping-list-entry').val('');
+    }
+
+    $('li').each(function () {
+      initializeItem($(this));
     })
 
     $(".filter").change(function() {
