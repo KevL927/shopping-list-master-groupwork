6c10a726025d8774ab1683a6326cb214c9d60d43
Kevin Lee <KevL927@users.noreply.github.com>
2016-08-23 23:02:14 -0400

    Working on refactoring code for filter

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var checkButton,
     deletedItems = [];
 
 $(document).ready(function() {
-  var itemTemplate = $('<li><span class="shopping-item"></span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></li>');
-  var shoppingList = $('.shopping-list')
+  var $itemTemplate = $('<li><span class="shopping-item"></span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></li>');
+  var $shoppingList = $('.shopping-list')
 
   // event handlers
 
@@ -15,62 +15,73 @@ $(document).ready(function() {
           return;
         }
 
-        var currentItem = itemTemplate.clone(),
-            currentShoppingItem = currentItem.find('.shopping-item');
+        var $currentItem = $itemTemplate.clone(),
+            $currentShoppingItem = $currentItem.find('.shopping-item');
 
-        currentShoppingItem.text($('#shopping-list-entry').val());
-        initializeItem(currentItem);
+        $currentShoppingItem.text($('#shopping-list-entry').val());
+        initializeItem($currentItem);
+        initializeFilterItem($currentItem);
     });
 
-    function initializeItem(currentItem) {
-      var currentShoppingItem = currentItem.find('.shopping-item');
-      var checkButton = currentItem.find('.shopping-item-toggle .button-label');
+    function initializeItem ($currentItem) {
+      var $currentShoppingItem = $currentItem.find('.shopping-item');
+      var $checkButton = $currentItem.find('.shopping-item-toggle .button-label');
 
-      function toggleItem (event, currentItem) {
-          currentShoppingItem.toggleClass('.shopping-item shopping-item__checked');
+      function toggleItem (event, $currentItem) {
+          $currentShoppingItem.toggleClass('.shopping-item shopping-item__checked');
           (checkButton.text() === 'check') ? checkButton.text('uncheck'): checkButton.text('check');
       }
       function deleteItem(event) {
-          currentItem.remove();
-          deletedItems.push(currentItem);
+          $currentItem.remove();
+          $deletedItems.push($currentItem);
       }
 
 
-      shoppingList.append(currentItem);
+      $shoppingList.append($currentItem);
 
 
-      $('.shopping-item-toggle', currentItem).on('click', toggleItem);
-      $('.shopping-item-delete', currentItem).on('click', deleteItem);
+      $('.shopping-item-toggle', $currentItem).on('click', toggleItem);
+      $('.shopping-item-delete', $currentItem).on('click', deleteItem);
 
 
       $('#shopping-list-entry').val('');
+
+      $(".filter").change(function() {
+          var filterSelect = $(this).val();
+
+          function hideUncheckedItem() {
+            $checkButton.filter(function() {
+                return $(this).text() === "uncheck"; }).closest('li').show();
+            $checkButton.filter(function() {
+                return $(this).text() === "check"; }).closest('li').hide();
+          }
+
+          function showCheckedItem() {
+            $checkButton.filter(function() {
+                return $(this).text() === "check"; }).closest('li').show();
+            $checkButton.filter(function() {
+                return $(this).text() === "uncheck"; }).closest('li').hide();
+          }
+
+          function showAllItem() {
+            $checkButton.filter(function() {
+                return $(this).text() === "uncheck"; }).closest('li').show();
+            $checkButton.filter(function() {
+                return $(this).text() === "check"; }).closest('li').show();
+          }
+
+          if (filterSelect === "Checked Items") {
+              hideUncheckedItem();
+          } else if (filterSelect === "Unchecked Items") {
+              showCheckedItem();
+          } else {
+              showAllItem();
+          }
+      })
     }
 
     $('li').each(function () {
       initializeItem($(this));
-    })
-
-    $(".filter").change(function() {
-        var filterSelector = $(this).val();
-
-        if (filterSelector === "Checked Items") {
-            $('.shopping-item-toggle .button-label').filter(function() {
-                return $(this).text() === "check"; }).closest('li').hide();
-            $('.shopping-item-toggle .button-label').filter(function() {
-                return $(this).text() === "uncheck"; }).closest('li').show();
-
-        } else if (filterSelector === "Unchecked Items") {
-            $('.shopping-item-toggle .button-label').filter(function() {
-                return $(this).text() === "uncheck"; }).closest('li').hide();
-            $('.shopping-item-toggle .button-label').filter(function() {
-                return $(this).text() === "check"; }).closest('li').show();
-
-        } else {
-            $('.shopping-item-toggle .button-label').filter(function() {
-                return $(this).text() === "uncheck"; }).closest('li').show();
-            $('.shopping-item-toggle .button-label').filter(function() {
-                return $(this).text() === "check"; }).closest('li').show();
-        }
     })
 
 });
